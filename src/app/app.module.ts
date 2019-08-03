6ecde9b5345c64fa04cce35e81db8d88befafb17
Filipe La Rocque <filipelarocque@gmail.com>
2019-08-03 19:34:43 -0300

    Componente favorito incluído no template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//  Icons
+import { IconsModule } from './modules/icons.module';
+
+//  Custom components
+import { FavoriteComponent } from './favorite/favorite.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
