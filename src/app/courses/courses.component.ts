e7280d443d8e0d2995a63bd9cf778c1a51d0504d
Filipe La Rocque <filipelarocque@gmail.com>
2019-08-03 17:44:14 -0300

    Serviço dos cursos criado

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CoursesService } from '../services/courses.service';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
+  providers: [CoursesService]
 })
 export class CoursesComponent implements OnInit {
 
   title: "The title of courses page";
   courses;
 
-  constructor() { }
+  constructor(coursesService: CoursesService) {
+    this.courses = coursesService.getCourses();
+  }
 
   ngOnInit() {
   }
