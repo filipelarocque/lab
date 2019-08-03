16393745a3ce88837aeaebe4877e773db4cac810
Filipe La Rocque <filipelarocque@gmail.com>
2019-08-03 17:48:53 -0300

    Componente dos autores criado

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+
+  title: "Authors";
+  authors;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
